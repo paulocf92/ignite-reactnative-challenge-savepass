408027c89b73b4aa79158d4cb81883fe5923dc25
Paulo Filho 3 <pcf.feat@gmail.com>
2021-11-07 11:39:26 -0400

    test

--- App.tsx
+++ App.tsx
@@ -5,28 +5,29 @@ import {
   useFonts,
   Rubik_300Light,
   Rubik_400Regular,
-  Rubik_500Medium
+  Rubik_500Medium,
 } from '@expo-google-fonts/rubik';
 import { NavigationContainer } from '@react-navigation/native';
 
 import { AppRoutes } from './src/routes/app.routes';
 
+// Test
 export default function App() {
   const [fontsLoaded] = useFonts({
     Rubik_300Light,
     Rubik_400Regular,
-    Rubik_500Medium
+    Rubik_500Medium,
   });
 
   if (!fontsLoaded) {
-    return <AppLoading />
+    return <AppLoading />;
   }
 
   return (
     <NavigationContainer>
       <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
+        barStyle='light-content'
+        backgroundColor='transparent'
         translucent
       />
       <AppRoutes />
